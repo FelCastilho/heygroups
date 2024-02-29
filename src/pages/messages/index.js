4847fc278b09a83681b343a2c83891cdfa227afd
felipeCast <castfelipe10@gmail.com>
2024-02-29 12:54:00 -0300

    feat: Criando estrutura das mensagens no firebase

--- src/pages/messages/index.js
+++ src/pages/messages/index.js
@@ -1,8 +1,52 @@
-import { Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 
 export default function Messages({ route }) {
 
-  const { threads } = route.params;
+  const { thread } = route.params;
+  const [ messages, setMessages ] = useState([]);
+
+  const user = auth().currentUser.toJSON();
+
+  useEffect(() => {
+
+    const unSubscriberListener = firestore().collection('MESSAGE_THREADS')
+    .doc(thread._id)
+    .collection('MESSAGES')
+    .orderBy('createdAt', 'desc')
+    .onSnapshot( querySnapshot => { 
+    //QuerySnapshot representa os elementos dentro de messages
+      const messages = querySnapshot.docs.map( doc => {
+        const firebaseData = doc.data();
+
+        const data = {
+          _id: doc.id,
+          text: '',
+          createdAt: firestore.FieldValue.serverTimestamp(),
+          ...firebaseData,
+        }
+
+        if(!firebaseData.system){
+          data.user = {
+            ...firebaseData.user,
+            name: firebaseData.user.displayName
+          }
+        }
+
+        return data;
+      })
+
+      setMessages(messages);
+      console.log(messages)
+    })
+
+    //Desmontando o componente ao sair da tela
+    return () => unSubscriberListener()
+
+  }, [])
 
   return (
     <View>
@@ -10,3 +54,7 @@ export default function Messages({ route }) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+
+})
